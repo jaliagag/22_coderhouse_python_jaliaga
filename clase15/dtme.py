e177c23347a9a858c2313d9ee8a57c1cd1b0d565
jaliagag <jmfaliaga@gmail.com>
2022-05-23 09:10:39 -0300

    finishing class 15

---
+++ clase15/dtme.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+dt = datetime(2000,1,1)
+print(dt)
---
+++ clase15/paquete_redistribuible/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='jalaiaga_primer_paquete',
+    version='1.0',
+    description='clase 15',
+    author='jaliaga',
+    authoer_email='jmfaliaga@gmail.com',
+
+    packages=['mi_primer_paquete']
+
+)
